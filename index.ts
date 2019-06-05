0d8d4c3024229d57cc3ba77861c3207c36da4ea9
Harminder virk <virk.officials@gmail.com>
2019-06-05 10:45:19 +0530

    feat: implement parser

---
+++ index.ts
@@ -0,0 +1,23 @@
+/**
+ * indicative-parser
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export { rulesParser, messagesParser } from './src/main'
+export {
+  ParsedRule,
+  ParsedMessages,
+  ParsedSchema,
+  ParsedFieldsMessages,
+  ParsedRulesMessages,
+  Schema,
+  SchemaNodeArray,
+  SchemaNodeLiteral,
+  SchemaNodeObject,
+  Message,
+  Messages,
+} from './src/contracts'
