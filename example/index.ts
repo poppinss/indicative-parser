edb6dd8f88c16a5504b706251571d57872eefb2f
Harminder virk <virk.officials@gmail.com>
2019-12-09 09:54:04 +0530

    docs: update API docs and example

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,18 @@
-import { rulesParser } from '..'
+import { rulesParser, t } from '..'
+import { deepEqual } from 'assert'
 
-console.log(JSON.stringify(rulesParser({
-  username: 'required',
-  'account.type': 'required|in:email,social',
-}), null, 2))
+const schema = rulesParser(t.schema({
+  username: t.string(),
+  account: t.object().members({
+    type: t.string({ name: 'in', args: ['email', 'social'] }),
+  }),
+}))
+
+const consiceSchema = rulesParser({
+  username: 'required|string',
+  'account': 'required|object',
+  'account.type': 'required|string|in:email,social',
+})
+
+deepEqual(schema, consiceSchema)
+console.log(JSON.stringify(schema, null, 2))
